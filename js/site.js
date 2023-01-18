c3e8d0d41492458ff0dc7c13f00654919e923470
Vann-K <114351642+Vann-K@users.noreply.github.com>
2023-01-18 15:06:04 -0500

    Initial Commit

--- js/site.js
+++ js/site.js
@@ -1,11 +1,46 @@
-function displayMessage() {
-    let msg = document.getElementById('message').value;
+// Logic Function
+// Reverse a string
+function reverseAString(userString) {
+    // Reverse the string
+    let revString = '';
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg
-        }
-    );
+    for (i = userString.length - 1; i >= 0; i--) {
+
+        revString += userString[i];
+
+    }
+
+    return revString;
+
+}
+
+// Entry Point aka controller
+function getValues() {
+
+    let inputString = document.getElementById('userString').value;
+
+    let reversedString = reverseAString(inputString);
+
+    displayString(reversedString);
+
+
+
+}
+
+
+
+
+function reverseAStringJS(userString) {
+    let reversed = userString.split('').reverse().join('');
+    // let userStringArray = userString.split(''); //'hello' => [h,e,l,l,o]
+    // let reversedArray = userStringArray.reverse(); // [h,e,l,l,o] => [o, l, l, e, h]
+}
+
+
+// view function
+function displayString(revString) {
+
+    document.getElementById('results').textContent = revString;
+    document.getElementById('alert').classList.remove('invisible');
+
 }
